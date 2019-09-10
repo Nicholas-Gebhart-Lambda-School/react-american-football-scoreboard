14451daf1040acc855722d33ed6ab392e5f44bcc
gebhartn <nicholas.gebhart@gmail.com>
2019-09-09 19:09:45 -0700

    Added Quarters button

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, setHomeScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
-  console.log(typeof awayScore);
 
   return (
     <div className="container">
@@ -20,7 +19,7 @@ function App() {
 
             <div className="home__score">{homeScore}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer">5:00</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score">{awayScore}</div>
